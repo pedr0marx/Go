2c2d38527474a68819d10895898d475f26ea8780
Pedro Marx <99131505+pedr0marx@users.noreply.github.com>
2024-11-17 19:15:55 -0300

    Add files via upload

---
+++ task-manager/cli/cli.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"example.com/tasks"
+)
+
+func main() {
+	// Carrega as tarefas do arquivo JSON
+	err := tasks.LoadTasksFromFile()
+	if err != nil {
+		fmt.Println("Erro ao carregar tarefas:", err)
+		return
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: task-manager [add|list|complete] [argumentos]")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+		case "add":
+			if len(os.Args) < 3 {
+				fmt.Println("Por favor, forneça a descrição da tarefa.")
+				return
+			}
+			description := os.Args[2]
+			tasks.AddTask(description)
+		case "list":
+			tasks.ListTasks()
+		case "complete":
+			if len(os.Args) < 3 {
+				fmt.Println("Por favor, forneça o ID da tarefa para completar.")
+				return
+			}
+			taskID := os.Args[2]
+			tasks.CompleteTask(taskID)
+		default:
+			fmt.Println("Comando não reconhecido.")
+	}
+}
---
+++ task-manager/cli/go.mod
@@ -0,0 +1,7 @@
+module example.com/cli
+
+go 1.23.3
+
+replace example.com/tasks => ../tasks
+
+require example.com/tasks v0.0.0-00010101000000-000000000000
